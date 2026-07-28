9b47ab5abe6e606ee5691d711d3af5a75f16e228
myname <myname@mymail.com>
2026-07-28 22:40:48 +0800

    fix: migrate kotlinOptions.jvmTarget to compilerOptions DSL for Kotlin 2.3.x
    Kotlin 2.3.x errors on 'jvmTarget = String' in kotlinOptions block.
    Use compilerOptions DSL with JvmTarget enum instead.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
     buildFeatures {
         compose = true
